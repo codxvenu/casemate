04c460852053df136ca23aacb3802c51b24a1269
codx_venu <vasulallu09@gmail.com>
2025-12-06 20:42:29 +0530

    fixed socket

--- app/context/SocketContext.js
+++ app/context/SocketContext.js
@@ -7,7 +7,12 @@ export const Socket = createContext(null);
 const SocketContext = ({children}) => {
     const [socket,setSocket] = useState(null);
     useEffect(() => {
-        const s = io(process.env.NEXT_PUBLIC_WEBSOCK_URL,{transports: ["websocket", "polling"],path: "/socket.io/"});
+    const s = io(process.env.NEXT_PUBLIC_WEBSOCK_URL, {
+      transports: ["websocket", "polling"],
+      path: "/socket.io/",
+      withCredentials: true,
+      secure: true,
+    });
         // const s = io("http://localhost:3001",{transports: ["websocket", "polling"],path: "/socket.io/"});
         setSocket(s);
         return () => {s.disconnect()}
